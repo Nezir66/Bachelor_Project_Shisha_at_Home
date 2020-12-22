f164d0d9fea78b4ce345ec088bf181020fab0d43
Nezir <Nezirbasar@outlook.com>
2020-12-22 22:06:51 +0100

    added footer

--- Client/layouts/default.vue
+++ Client/layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '../components/footer.vue'
 export default {
+  components: {
+    Footer
+  }
 };
 </script>
 
