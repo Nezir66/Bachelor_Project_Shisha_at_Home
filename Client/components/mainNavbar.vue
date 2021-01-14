81dfaafe3813c9330e782ab9bcf03d07a6a83fe4
Nezir <Nezirbasar@outlook.com>
2021-01-14 15:12:23 +0100

    Done: new navbar, useable with tab, responsive

--- Client/components/mainNavbar.vue
+++ Client/components/mainNavbar.vue
@@ -1,168 +1,208 @@
 <template>
   <body class="top_section cross_layout">
-    <!-- section -->
-    <section id="home">
-      <div class="row">
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 Sidebar" tabindex="0">
-          <transition name="slide">
-            <div class="sideBarNew" v-if="sidebarOpen">
-              <div class="sidebarMenü">
-                <router-link
-                  to="/Account/signIn"
-                  href="#"
-                  style="color: white; font-size: 40px"
-                  >Einloggen</router-link
-                >
-              </div>
-              <hr style="background-color: white" />
-              <router-link to="/Transaction/Offer/products" href="#">
-                <div class="sidebarContent">Angebot</div>
-              </router-link>
-              <router-link
-                to="/Account/profil"
-                href="#"
-                v-if="$store.state.auth.loggedIn == true"
-              >
-                <div class="sidebarContent">Profil</div>
-              </router-link>
-              <router-link to="/Transaction/cart" href="#">
-                <div class="sidebarContent">Warenkorb</div>
-              </router-link>
-              <router-link to="/Game/chooseGame" href="#">
-                <div class="sidebarContent">Shisha-Spiel</div>
-              </router-link>
-              <router-link to="/Transaction/calculator" href="#">
-                <div class="sidebarContent">Kalkulator</div>
-              </router-link>
-              <router-link to="/Transaction/tutorial" href="#">
-                <div class="sidebarContent">Erklärung</div>
-              </router-link>
-              <a
-                class="navContent"
+    <nav>
+      <input type="checkbox" id="click" />
+      <label for="click" class="menu-btn">
+        <i class="fa fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <router-link to="/" aria-label="zur Startseite zurückkehren"
+            >Startseite</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/Transaction/Offer/products" href="#"
+            >Angebot</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/Transaction/cart" href="#">Warenkorb</router-link>
+        </li>
+        <li>
+          <router-link to="/Game/quiz" href="#">Shisha-Spiel</router-link>
+        </li>
+        <li>
+          <router-link
+            id="sidebarCollapse"
+            to="/Account/profil"
+            class="active"
+            v-if="$auth.$state.loggedIn == true"
+            aria-label="Profil anschauen"
+          >
+            {{ $auth.$state.user.name }}&nbsp;&nbsp;
+            <i class="fa fa-user-circle" style="transform: scale(2)"></i>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/Transaction/calculator" href="#"
+            >Kalkulator</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/Transaction/tutorial" href="#"
+            >Erklärung</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/Account/profil"
+            v-if="$auth.$state.loggedIn == true"
+            aria-label="Profil anschauen"
+            href="#"
+            >Profil</router-link
+          >
+        </li>
+        <li><router-link to="/Account/signIn" href="#" v-if="$auth.$state.loggedIn == false">Einloggen</router-link></li>
+        <li><a
                 href="#"
                 v-show="$auth.$state.loggedIn == true"
                 @click="logOut()"
               >
-                <div class="sidebarContent">Ausloggen</div>
-              </a>
-            </div>
-          </transition>
-        </div>
+                Ausloggen
+              </a></li>
+      </ul>
+    </nav>
+    <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-          <!-- header -->
-          <header>
-            <!-- header inner -->
-            <div class="container-fluid">
-              <div class="row">
-                <div
-                  class="col-lg-3 col col-xl-3 col-sm-3 col-md-3 col-xs-3"
-                  style="margin-top: 20px"
-                >
-                  <a
-                    href="#"
-                    @keyup.enter="showSidebar"
-                    aria-label="Seiten Menü anzeigen"
-                  >
-                    <i
-                      class="fa fa-bars header-all"
-                      style="transform: scale(3); cursor: pointer; float: left"
-                      @click="showSidebar"
-                    ></i>
-                  </a>
-                </div>
-                <div class="col-lg-9 col col-xl-9 col-sm-9 col-md-9 col-xs-9">
-                  <div class="right_header_info">
-                    <router-link to="/" aria-label="zur Startseite zurückkehren"
-                      ><i class="fa fa-home home-button header-all"></i
-                    ></router-link>
-                    <router-link
-                      to="/Transaction/cart"
-                      class="text-center header-content header-all"
-                      style="margin-top: -20px"
-                      :aria-label="
-                        'Gesamtprodukte im Einkaufswagen ' +
-                        $store.state.totalProduct
-                      "
-                    >
-                      {{ $store.state.totalProduct }}
-                      <br />
-                      <i
-                        class="fa fa-shopping-cart"
-                        style="transform: scale(2.5)"
-                      ></i>
-                    </router-link>
-
-                    <router-link
-                      id="sidebarCollapse"
-                      to="/Account/profil"
-                      class="header-content header-all"
-                      v-if="$auth.$state.loggedIn == true"
-                      aria-label="Profil anschauen"
-                    >
-                      {{ $auth.$state.user.name }}&nbsp;&nbsp;
-                      <i
-                        class="fa fa-user-circle"
-                        style="transform: scale(2)"
-                      ></i>
-                    </router-link>
-                    <router-link
-                      to="/"
-                      class="header-content header-all"
-                      href="#"
-                      v-else
-                      aria-label="Anmelden um Profil anzuschauen"
-                    >
-                      <i
-                        class="fa fa-user-circle"
-                        style="transform: scale(2)"
-                      ></i>
-                    </router-link>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- end header inner -->
-          </header>
-          <slot></slot>
-          <!-- end header -->
+        <slot style="margin-top:200px;"></slot>
         </div>
-      </div>
-    </section>
-    <!-- end section -->
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+    </div>
+    
   </body>
-</template>   
+</template>
 
 <script>
 export default {
-  data() {
-    return {
-      sidebarOpen: false,
-    };
-  },
   methods: {
     async logOut() {
       await this.$auth.logout();
       this.$router.go(0);
-    },
-    showSidebar() {
-      this.sidebarOpen = !this.sidebarOpen;
-    },
+    }
   },
 };
 </script>
 
 <style scopped>
-.home-button {
-  color: white;
-  transform: scale(3);
-  margin-left: 30%;
+@import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+nav {
+  display: flex;
+  height: 80px;
+  width: 100%;
+  background: linear-gradient(90deg, rgba(0,0,0,0.2),rgba(130,38,14,0.5),rgba(0,0,0,0.2));
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px 0 100px;
+  flex-wrap: wrap;
+}
+nav .logo {
+  color: #fff;
+  font-size: 35px;
+  font-weight: 600;
+}
+nav ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: auto;
+}
+nav ul li {
+  margin: 0 5px;
+}
+nav ul li a{
+  color: #f2f2f2;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 8px 15px;
+  border-radius: 5px;
+  letter-spacing: 1px;
+  transition: all 0.3s ease;
+}
+nav ul li a.active,
+nav ul li a:hover,
+nav ul li a:focus {
+  color: #82260e;
+  background: #fff;
+}
+nav .menu-btn i {
+  color: #fff;
+  font-size: 22px;
+  cursor: pointer;
+  display: none;
+}
+input[type="checkbox"] {
+  display: none;
 }
 
-@media (max-width: 575px) {
-  .home-button {
-    margin-left: -40%;
+@media (max-width: 1000px) {
+  nav {
+    padding: 0 40px 0 50px;
+  }
+}
+@media (max-width: 920px) {
+  nav {
+    background: rgba(0,0,0,0);
+  }
+
+  nav .menu-btn i {
+    display: block;
+  }
+  #click:checked ~ .menu-btn i:before {
+    content: "\f00d";
+  }
+  nav ul {
+    position: fixed;
+    top: 80px;
+    left: -100%;
+    background:  #82260e;
+    height: 100vh;
+    width: 100%;
+    text-align: center;
+    display: block;
+    transition: all 0.3s ease;
+    z-index: 999;
+  }
+  #click:checked ~ ul {
+    left: 0;
+  }
+  nav ul li {
+    width: 100%;
+    margin: 20px 0;
+  }
+  nav ul li a {
+    width: 100%;
+    margin-left: -100%;
+    display: block;
+    font-size: 20px;
+    transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  }
+  #click:checked ~ ul li a {
+    margin-left: 0px;
+  }
+  nav ul li a.active,
+  nav ul li a:hover {
+    background: none;
+    color: white;
+  }
+}
+@media (max-height: 680px) {
+  nav ul li {
+    margin: 15px 0;
+  }
+}
+
+@media (max-height: 580px) {
+  nav ul li {
+    margin: 7px 0;
   }
 }
 </style>
-      
-      
