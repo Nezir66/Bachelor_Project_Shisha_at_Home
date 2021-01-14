a7c7efde716c1065cb8bed862fc12499a57d089c
Nezir <Nezirbasar@outlook.com>
2021-01-14 15:13:21 +0100

    deleted unused packages

--- Client/nuxt.config.js
+++ Client/nuxt.config.js
@@ -13,7 +13,6 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: '/css/style.css' },
-      { rel: 'stylesheet', href: '/css/bootstrap.min.css' },
       { rel: 'stylesheet', href: '/css/responsive.css' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' },
     ]
